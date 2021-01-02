ae508a17b5f2fe370e1a819fdcf4ad739f0b4f25
ndriqa <ndriqimrrahmani@hotmail.com>
2021-01-02 23:23:12 +0100

    modify index

--- js/nightmode.js
+++ js/nightmode.js
@@ -1,10 +1,30 @@
-$(document).ready(function () {
-    var url = window.location.href;
-    let me = $('ul#menu li a');
-    $(me).map(function (idx, elem) {
-        if (url.includes($(elem).attr("href"))) {
-            $(elem).addClass("nav-link-active");
-        }
-    });
+function updateBackground() {
+    var 
+    hr = (new Date()).getHours(),
+    body = document.body,
+    bstyle = body.style,
+    headertopstyle = document.querySelector('.social-top'),
+    headertitle = document.querySelector('.homepage-description h1'),
+    headerdescription = document.querySelector('.homepage-description p'),
+    footer = document.querySelector('.main-footer');
+    
+    if (hr >= 18 || hr < 8) {
+        document.querySelectorAll(".night-text").forEach(element => {
+            element.style.color = "#fff";
+        });
+        document.querySelectorAll(".night-background").forEach(element => {
+            element.style.color = "#000";
+        });
+
+    } else if (hr > 8 && hr < 19) {
+        document.querySelectorAll(".night-text").forEach(element => {
+            element.style.color = "#000";
+        });
+        document.querySelectorAll(".night-background").forEach(element => {
+            element.style.color = "#fff";
+        });
+    }
 }
-);
+
+setInterval(updateBackground, 1000 * 60);
+updateBackground();
