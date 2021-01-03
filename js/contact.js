f1b5548cd8733573523609da1958ec5aaf43ddce
Urata Sadiku <sadikuurata@gmail.com>
2021-01-03 18:02:07 +0100

    fixed bugs

--- js/contact.js
+++ js/contact.js
@@ -22,9 +22,10 @@ const PHONE_REGEX_PATTERN = /^[+][/0-9]{8,12}$/gm;
 
 function cashout() {   
     let name = document.getElementById("name").value; 
+    let lname = document.getElementById("lname").value;
     let phone = document.getElementById("phone").value;
     console.log(phone);
-    if (!name){
+    if (!name || !lname){
         window.alert("Please write first and last name")
     } else if (!PHONE_REGEX_PATTERN.test(phone)) {
         //qite naj pop-up qe gabim eshte numri i tel
